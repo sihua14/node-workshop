e58741593d5fdbf1f4c9b72a8ff349d6dce477f2
sihua <yaya14chen@gmail.com>
2021-07-31 12:39:23 +0800

    modify sum.js

---
+++ basic/sum.js
@@ -0,0 +1,14 @@
+// console.log(123);
+
+function number(n){
+    let sum=0;
+    for (let i=0;i<n;i++){
+        sum= sum + i 
+
+    }
+    return sum;
+}
+
+console.log(number(5));
+console.log(number(100));
+console.log(number(500));
