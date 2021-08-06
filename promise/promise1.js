307a87039c0c3c412294e856f13f78e037076f62
sihua <yaya14chen@gmail.com>
2021-08-07 00:34:27 +0800

    再練習promise1.js

--- promise/promise1.js
+++ promise/promise1.js
@@ -4,24 +4,30 @@ let doWork = function (job, timer, isOK) {
         setTimeout(() => {
           let dt = new Date();
           if (isOK) {
-              resolve(`完成工作: ${job} at ${dt.toISOString()}`);
+            resolve(`完成工作: ${job} at ${dt.toISOString()}`);
         } else {
-          reject(`失敗了 ${job}`);
+            reject(`失敗了 ${job}`);
         }
       }, timer);
     });
   };
 
+
 let dt = new Date();
 console.log(`開始工作 at ${dt.toISOString()}`);
 
-let job1 = doWork("刷牙", 3000, false);
-console.log(job1);
+
+let job1 = doWork("刷牙", 3000, false); 
+//doWork回傳的是promise物件
+console.log(job1); // =>pending
+//用then()來接收「然後」的結果
 job1.then(
   function (resolve) {
+    //準備接收成功
     console.log("第 1 個函式被呼叫了", resolve);
   },
   function (reject) {
+    //準備接收失敗
     console.log("第 2 個函式被呼叫了", reject);
   }
 );
