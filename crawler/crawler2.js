486e002c76c63ffbaceff5569918b3ada53d70d5
sihua <yaya14chen@gmail.com>
2021-08-08 01:32:00 +0800

    用promise改寫crawler1.js(兩種寫法)

--- crawler/crawler2.js
+++ crawler/crawler2.js
@@ -1,14 +1,15 @@
 /*
-先讀到檔案stockNo＝>fs.readFile
-讀完檔案後才會有stockNumber
-然後axios拿到這個股票代號後
-再發出請求
+先讀到檔案stockNo＝>fs.readFile -> 讀完檔案後才會有stockNumber
+然後axios拿到這個股票代號後 -> 再發出請求 ->最後得到3092鴻碩的股票
 */
 
 const axios = require("axios");
 const moment = require("moment");
 const fs = require("fs");
 
+
+
+//寫法二（比較好看）
 new Promise((resolve, reject) => {
     fs.readFile("stockNo.txt","utf8",(err,stockNum)=>{
         if (err) {
@@ -19,23 +20,50 @@ new Promise((resolve, reject) => {
      });
     })
     .then((stockNum)=>{
-    axios.get("https://www.twse.com.tw/exchangeReport/STOCK_DAY",{
-        params:{
-            response: "json",
-            date:moment().format("YYYYMMDD"),
-            stockNo:stockNum,
-            },
-        })
-        .then ((response) =>{
+        return axios.get("https://www.twse.com.tw/exchangeReport/STOCK_DAY",{
+          params:{
+          response: "json",
+          date:moment().format("YYYYMMDD"),
+          stockNo:stockNum,
+          },
+        })      
+    })       
+    .then((response) =>{
             console.log(response.data.title);
-        }) 
-        .catch((error)=>{
+    }) 
+    .catch((err)=>{
             console.log(err);
-        })
     })
 
-
-
-
      
+
+
+
+//寫法一（有點像callbacl hell）：
+
+// new Promise((resolve, reject) => {
+//     fs.readFile("stockNo.txt","utf8",(err,stockNum)=>{
+//         if (err) {
+//             reject(err);           
+//         }else{
+//             resolve(stockNum)
+//         }
+//      });
+//     })
+//     .then((stockNum)=>{ 
+       //這裡的參數也可以用其他命名，因為是從resolve拿到的，所以不一定要是stockNum
+//     axios.get("https://www.twse.com.tw/exchangeReport/STOCK_DAY",{
+//         params:{
+//             response: "json",
+//             date:moment().format("YYYYMMDD"),
+//             stockNo:stockNum,
+//             },
+//         })
+//         .then ((response) =>{
+//             console.log(response.data.title);
+//         }) 
+//         .catch((error)=>{
+//             console.log(err);
+//         })
+//     })
 
