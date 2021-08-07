e266105b95d147df3da31daa071c21a1d673ab73
sihua <yaya14chen@gmail.com>
2021-08-08 05:22:57 +0800

    修改挑戰題用Await改寫！

--- crawler/crawlerAwait.js
+++ crawler/crawlerAwait.js
@@ -1,9 +1,11 @@
+//太激動了！！不知道對不對
 
 const axios = require("axios");
 const moment = require("moment");
 const fs = require("fs");
 
 async function goodJob() {
+try{   
     let stock = await new Promise((resolve, reject) => {
         fs.readFile("stockNo.txt","utf8",(err,stockNum)=>{
             if (err) {
@@ -12,16 +14,20 @@ async function goodJob() {
                 resolve(stockNum)
             }
          });
-        })
+    })
     let result = await axios.get("https://www.twse.com.tw/exchangeReport/STOCK_DAY",{
         params:{
         response: "json",
         date:moment().format("YYYYMMDD"),
         stockNo:stock,
         },
-        })      
+     })      
     console.log(result.data.title);
-
+    }
+    
+    catch(error){
+         console.log(error);
+    }
  };
  goodJob() 
     
