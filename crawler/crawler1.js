6b2239feb04d57c9ecf82d005481a9566b42677b
sihua <yaya14chen@gmail.com>
2021-08-07 16:09:29 +0800

    add crawler1.js

---
+++ crawler/crawler1.js
@@ -0,0 +1,15 @@
+const axios = require("axios");
+const moment = require("moment")
+
+// console.log(momoent().format("YYYYMMDD"));
+axios.get("https://www.twse.com.tw/exchangeReport/STOCK_DAY",{
+    params:{
+            response: "json",
+            date:moment,
+            stockNo:"2330",
+    },
+   })
+    .then ((response) =>{
+        console.log(response.data);
+
+    })
