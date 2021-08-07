468f307cdc12a5bfd3f2fb20ab1aa09febd26a9b
sihua <yaya14chen@gmail.com>
2021-08-08 01:36:31 +0800

    用promise改寫crawler1.js(兩種寫法)

--- crawler/crawler2.js
+++ crawler/crawler2.js
@@ -1,5 +1,5 @@
 /* 
-特別注意：stockNo.txt 填入股票代號時，不能換到第二行，否則會get到的事股票代號跟一行空行，這樣就會get不到資料，node 之後會出現undefined 
+特別注意：stockNo.txt 填入股票代號時，不能換到第二行，否則會get到的會是股票代號跟一行空行，這樣就會get不到資料，node 之後會出現undefined 
 */
 
 /*
