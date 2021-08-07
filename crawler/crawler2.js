a25b4fe7509c29f745d69e44cf411e49da475670
sihua <yaya14chen@gmail.com>
2021-08-08 01:41:00 +0800

    用promise改寫crawler1.js(兩種寫法)-修改註解

--- crawler/crawler2.js
+++ crawler/crawler2.js
@@ -1,5 +1,6 @@
 /* 
-特別注意：stockNo.txt 填入股票代號時，不能換到第二行，否則會get到的會是股票代號跟一行空行，這樣就會get不到資料，node 之後會出現undefined 
+特別注意：stockNo.txt 填入股票代號時，不能換到第二行，否則會get到的會是股票代號跟一行空行，這樣就會get不到資料，node 之後會出現undefined。
+或者是用 https://developer.mozilla.org/zh-TW/docs/Web/JavaScript/Reference/Global_Objects/String/Trim 的方法，使用 ".trim()" 把空行過濾掉。
 */
 
 /*
