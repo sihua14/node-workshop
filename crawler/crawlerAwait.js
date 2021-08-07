ea8072b56924396ace4c1471fd6b7a063c578f45
sihua <yaya14chen@gmail.com>
2021-08-08 05:10:28 +0800

    完成用await改寫Q Q

---
+++ crawler/crawlerAwait.js
@@ -0,0 +1,27 @@
+
+const axios = require("axios");
+const moment = require("moment");
+const fs = require("fs");
+
+async function goodJob() {
+    let stock = await new Promise((resolve, reject) => {
+        fs.readFile("stockNo.txt","utf8",(err,stockNum)=>{
+            if (err) {
+                reject(err);           
+            }else{
+                resolve(stockNum)
+            }
+         });
+        })
+    let result = await axios.get("https://www.twse.com.tw/exchangeReport/STOCK_DAY",{
+        params:{
+        response: "json",
+        date:moment().format("YYYYMMDD"),
+        stockNo:stock,
+        },
+        })      
+    console.log(result.data.title);
+
+ };
+ goodJob() 
+    
