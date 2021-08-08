029b0e17061f07e8f02e11445e1fb18c94d2d9d1
sihua <yaya14chen@gmail.com>
2021-08-08 09:04:22 +0800

    排版

--- crawler/crawlerAwait.js
+++ crawler/crawlerAwait.js
@@ -5,29 +5,29 @@ const moment = require("moment");
 const fs = require("fs");
 
 async function goodJob() {
-try{   
-    let stock = await new Promise((resolve, reject) => {
-        fs.readFile("stockNo.txt","utf8",(err,stockNum)=>{
-            if (err) {
-                reject(err);           
-            }else{
-                resolve(stockNum)
-            }
-         });
-    })
-    let result = await axios.get("https://www.twse.com.tw/exchangeReport/STOCK_DAY",{
-        params:{
-        response: "json",
-        date:moment().format("YYYYMMDD"),
-        stockNo:stock,
-        },
-     })      
-    console.log(result.data.title);
-    }
     
+    try{
+        let stock = await new Promise((resolve, reject) => {
+            fs.readFile("stockNo.txt","utf8",(err,stockNum)=>{
+                if(err){
+                    reject(err);           
+                }else{
+                    resolve(stockNum)
+                }
+            });
+        })
+        let result = await axios.get("https://www.twse.com.tw/exchangeReport/STOCK_DAY",{
+            params:{
+            response: "json",
+            date:moment().format("YYYYMMDD"),
+            stockNo:stock,
+            },
+        })      
+        console.log(result.data.title);
+        }
     catch(error){
-         console.log(error);
-    }
- };
+        console.log(error);
+        }
+    };
  goodJob() 
     
