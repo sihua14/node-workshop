f12dee567fd8c14703140c5e229d1ef380423c7a
sihua <yaya14chen@gmail.com>
2021-08-14 01:05:30 +0800

    crawler-callback版本

--- crawler/crawler-db.js
+++ crawler/crawler-db.js
@@ -1,51 +1,66 @@
+//callback版
 const axios = require("axios");
 const moment = require("moment");
 const fs = require("fs");
+const mysql = require("mysql");//第三方，需要npm i mysql
 require("dotenv").config();
 
-const mysql = require("mysql");
-const { request } = require("http");
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  port: process.env.DB_PORT, 
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME, 
+});
 
-  const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    port: process.env.DB_PORT,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+connection.connect((err) => {
+  if (err) {
+    console.error("資料庫連不上",err);
+  }
+});
+
+
+async function goodJob() {
+  let stock = await new Promise((resolve, reject) => {
+    fs.readFile("stockNo.txt","utf8",(err,stockNum)=>{
+      if(err){
+          reject(err);           
+      }else{
+          resolve(stockNum.trim()); //trim()過濾掉資料的空白字元->過安全
+      }
+    });
   });
+ 
 
-  connection.connect((err) => {
-    if (err) {
-      console.error("資料庫連不上",err);
+  // 查詢資料庫的callback版本
+  connection.query(
+    "SELECT * FROM stock WHERE stock_id = ?", [stock],
+    function (error, results, fields){
+      if (error) throw error;
+      if (results !== 0) {
+        console.log("有資料");
+      }
     }
-  });
+  );
 
-  // 不關閉連線，認為程式一直在執行
-  connection.end();
+  let response = await axios.get(
+    "https://www.twse.com.tw/exchangeReport/STOCK_DAY",
+    {
+      params:{
+      response: "json",
+      date:moment().format("YYYYMMDD"),
+      stockNo:stock,
+      },
+    }
+  );   
+  console.log(response.data.title);
+};      
 
-// async function goodJob() {
-    
-    // try{
-    //     let stock = await new Promise((resolve, reject) => {
-    //         fs.readFile("stockNo.txt","utf8",(err,stockNum)=>{
-    //             if(err){
-    //                 reject(err);           
-    //             }else{
-    //                 resolve(stockNum.trim()) //trim()過濾掉資料的空白字元->過濾掉較安全
-    //             }
-    //         });
-    //     })
-//         let result = await axios.get("https://www.twse.com.tw/exchangeReport/STOCK_DAY",{
-//             params:{
-//             response: "json",
-//             date:moment().format("YYYYMMDD"),
-//             stockNo:stock,
-//             },
-//         })      
-//         console.log(result.data.title);
-//         }
-//     catch(error){
-//         console.log(error);
-//         }
-//     };
-//  goodJob() 
+
+goodJob();
+
+ 
+
+  
+
+  
