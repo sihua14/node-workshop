f734d10cb47e89adc1f58b4811e485adfdf123e1
sihua <yaya14chen@gmail.com>
2021-08-14 14:00:11 +0800

    cat.js

---
+++ module/cat.js
@@ -0,0 +1,28 @@
+let brand = "貓貓之家"
+let color = "奶牛"
+let weight = "5公斤"; 
+let name = "小奶牛";
+
+function getBrand() {
+    return brand;
+}
+
+function getColor() {
+    return color;
+}
+
+function getWeight() {
+    return weight;
+}
+
+
+function getName() {
+    return name;
+}
+
+module.exports = {
+    getBrand,
+    getColor,
+    getWeight,
+    getName,   
+}
---
+++ module/index.js
@@ -0,0 +1,5 @@
+
+const cat = require("./cat");
+
+// console.log(cat);
+console.log(cat.getBrand());
