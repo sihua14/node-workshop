3c9de0b4943bc383633ada0bceaeb10c87d602cc
sihua <yaya14chen@gmail.com>
2021-08-15 10:43:25 +0800

    server.js

--- simple-express/server.js
+++ simple-express/server.js
@@ -8,13 +8,10 @@ app.get("/", function (request, response, next) {
   response.send("Hello");
 });
 
-app.listen(3000, function () {
-  console.log("我們的 web server 啟動了～");
-});
-
 app.get("/about", function (request, response, next) {
     response.send("hi");
 });
+
 app.listen(3000, function () {
-    console.log("關於我們");
-})
+  console.log("我們的 web server 啟動了～");
+});
