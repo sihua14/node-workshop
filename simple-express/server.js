ed5e888c0c5ef2c67e3736661ca532235630d135
sihua <yaya14chen@gmail.com>
2021-08-15 11:33:24 +0800

    add middleware

--- simple-express/server.js
+++ simple-express/server.js
@@ -1,12 +1,24 @@
+const { DESTRUCTION } = require("dns");
 const express = require("express");
 
 // 利用 express 建立了一個 express application
 let app = express();
 
+app.use((req, res, next) => {
+    console.log("我是第一名");
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log("我是第二名");
+    next();
+});
+
 // HTTP Method: get, post, put, patch, delete
 app.get("/", function (request, response, next) {
   response.send("Hello");
 });
+
 
 app.get("/about", function (request, response, next) {
     response.send("hi");
